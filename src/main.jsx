28129ff09608d1570fb417adfa965a34c81646b8
Vasu3050 <23u0707@students.git.edu>
2026-02-25 19:36:39 +0530

    fix refresh logout bug

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store/store.js";
 
 import "./index.css";
 import store from "./store/store.js";
@@ -71,7 +73,7 @@ const router = createBrowserRouter([
           { path: "approve-users", element: <ApproveUsers /> },
           { path: "classes", element: <ManageClasses /> },
           { path: "calendar", element: <CalendarManagement /> },
-          {path: "year", element: <AcademicYear/>}
+          { path: "year", element: <AcademicYear /> }
         ],
       },
 
@@ -109,9 +111,11 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
